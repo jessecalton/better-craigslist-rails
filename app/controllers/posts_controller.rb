a6fb79ebd303ef48a8f5472ecd817f9f90bdc2a8
Dylan Cordoni & Jesse Calton <pair+dylancordoni.jessecalton@devbootcamp.com>
2017-03-28 18:39:03 -0700

    adding posts works

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+    if @category
+      @posts = @category.posts
+    else
+      redirect_to '/'
+    end
+  end
+
+  def new
+    @post = Post.new
+    @category = Category.find(params[:category_id])
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @post = Post.new(post_params)
+    @post.category_id = @category.id
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :author)
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# categories = []
+# 10.times {Category.create(name: Faker::GameOfThrones.city, description: Faker::GameOfThrones.character)}
+10.times {Category.create(name: Faker::GameOfThrones.city, description: Faker::GameOfThrones.character)}
+categories = Category.all
+categories.each do |category|
+  post = Post.new(title: Faker::GameOfThrones.city, body: Faker::GameOfThrones.city, author: Faker::GameOfThrones.city)
+  category.posts << post
+end
